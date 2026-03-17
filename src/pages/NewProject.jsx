b52aa7f1032d897625223924b4ae6d809165be2a
sart55 <105958020+sart55@users.noreply.github.com>
2026-03-18 01:25:14 +0530

    Update NewProject.jsx

--- src/pages/NewProject.jsx
+++ src/pages/NewProject.jsx
@@ -1,6 +1,6 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
-import CommentSection from "../components/Commentsection";
+import CommentSection from "../components/CommentSection";
 import PaymentForm from "../components/PaymentForm";
 import CustomerDetails from "../components/CustomerDetails";
 import QuotationHistory from "../components/QuotationHistory";
